72801b0524ac51a318cd8761447f1472ab61f6d9
Alan Rempel <remp0017@algonquinlive.com>
2022-11-08 14:28:10 -0500

    Create get_file_content and corresponding test

--- AlvinS_AlanR/GIS4107_Week10_starter/file_utils.py
+++ AlvinS_AlanR/GIS4107_Week10_starter/file_utils.py
@@ -1,2 +1,6 @@
 def get_file_content(file_name):
-    return None
+    try:
+        with open(file_name) as file:
+            return file.readline()
+    except FileNotFoundError:
+        return file_name + " does not exist"
--- AlvinS_AlanR/GIS4107_Week10_starter/test_file_utils.py
+++ AlvinS_AlanR/GIS4107_Week10_starter/test_file_utils.py
@@ -1,2 +1,14 @@
-def test_get_file_content():
-    
+import os
+
+import file_utils
+
+def test_get_file_content_existing():
+    script_folder = os.path.dirname(os.path.abspath(__file__))
+    file_name = os.path.join(script_folder, "data", "demo.txt")
+    content = file_utils.get_file_content(file_name)
+    assert content == "Alan and Alvin"
+
+def test_get_file_content_nonexistent():
+    file_name = "nonexistent.txt"
+    result = file_utils.get_file_content(file_name)
+    assert result == "nonexistent.txt does not exist"
